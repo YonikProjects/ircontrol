7baf194ca0d488423539b6e11eac2d6e1e10b6d8
Yonik <dimap55@gmail.com>
2023-03-29 22:00:09 +0300

    Fixed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 const JSONdb = require("simple-json-db");
 const { SerialPort } = require("serialport");
@@ -16,27 +16,7 @@ if (app.isPackaged) {
   const server = "https://ircontrol-updater-oxcgutzi0-yonikprojects.vercel.app";
   const url = `${server}/update/${process.platform}/${app.getVersion()}`;
   autoUpdater.setFeedURL(url);
-  setInterval(() => {
-    autoUpdater.checkForUpdates();
-  }, 60000);
-  autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
-    const dialogOpts = {
-      type: "info",
-      buttons: ["Restart", "Later"],
-      title: "Application Update",
-      message: process.platform === "win32" ? releaseNotes : releaseName,
-      detail:
-        "A new version has been downloaded. Restart the application to apply the updates.",
-    };
-
-    dialog.showMessageBox(dialogOpts).then((returnValue) => {
-      if (returnValue.response === 0) {
-        autoUpdater.quitAndInstall();
-        app.closable = true;
-        app.exit(0);
-      }
-    });
-  });
+  autoUpdater.checkForUpdates();
 }
 
 function handleSquirrelEvent() {
