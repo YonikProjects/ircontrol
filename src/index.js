c5a096598318dec2829f08cbbb7f0ce5b06bb46d
Yonik <dimap55@gmail.com>
2023-03-29 22:42:01 +0300

    Fixed

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ if (app.isPackaged) {
     console.error("There was a problem updating the application");
     console.error(message);
   });
-  const server = "https://ircontrol-updater-oxcgutzi0-yonikprojects.vercel.app";
+  const server = "https://nuts.dmitrypol.com";
   const url = `${server}/update/${process.platform}/${app.getVersion()}`;
   autoUpdater.setFeedURL(url);
   autoUpdater.checkForUpdates();
