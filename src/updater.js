953bad1ddf0cf972e06b1ab445b077066221dd5c
Yonik <dimap55@gmail.com>
2023-07-10 08:31:43 +0300

    Fixed

--- src/updater.js
+++ src/updater.js
@@ -1,28 +1,31 @@
-const { app } = require("electron");
+// const { app } = require("electron");
 const installer = require("./installer");
 
 async function updater() {
   await installer.installer();
-  console.log("Updater started");
-  if (app.isPackaged) {
-    const { autoUpdater } = require("electron");
-    autoUpdater.on("error", (message) => {
-      console.error("There was a problem updating the application");
-      console.error(message);
-    });
-    const server = "https://icontrol.vercel.app";
-    const url = `${server}/update/${process.platform}/${app.getVersion()}`;
-    autoUpdater.setFeedURL(url);
-    autoUpdater.checkForUpdates();
-    setInterval(() => {
-      autoUpdater.checkForUpdates();
-    }, 90000);
-    autoUpdater.on("update-downloaded", () => {
-      autoUpdater.quitAndInstall();
-      app.closable = true;
-      app.exit(0);
-    });
-  }
+
+  //  disabled updates because i'm leaving gordon
+
+  // console.log("Updater started");
+  // if (app.isPackaged) {
+  //   const { autoUpdater } = require("electron");
+  //   autoUpdater.on("error", (message) => {
+  //     console.error("There was a problem updating the application");
+  //     console.error(message);
+  //   });
+  //   const server = "https://icontrol.vercel.app";
+  //   const url = `${server}/update/${process.platform}/${app.getVersion()}`;
+  //   autoUpdater.setFeedURL(url);
+  //   autoUpdater.checkForUpdates();
+  //   setInterval(() => {
+  //     autoUpdater.checkForUpdates();
+  //   }, 90000);
+  //   autoUpdater.on("update-downloaded", () => {
+  //     autoUpdater.quitAndInstall();
+  //     app.closable = true;
+  //     app.exit(0);
+  //   });
+  // }
 }
 
 module.exports = {
